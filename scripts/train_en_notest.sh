eb01e09818a0eb5cc5806e7c384562d9ef4e2420
unikcc <19505579+unikcc@users.noreply.github.com>
2023-06-08 21:13:58 +0800

    update dataset

--- scripts/train_en_notest.sh
+++
@@ -1,5 +0,0 @@
-python main.py \
-    --lang en \
-    --cuda_index 0 \
-    --bert_lr 1e-5 \
-    --input_files "train valid"
--- scripts/train_zh_notest.sh
+++
@@ -1,5 +0,0 @@
-python main.py \
-    --lang zh \
-    --cuda_index 0 \
-    --bert_lr 1e-5 \
-    --input_files "train valid"
